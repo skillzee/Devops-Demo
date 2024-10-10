8c6e13a2494872f45bafdb9dddc8001f9f7bf457
Parth Sharma <112481963+skillzee@users.noreply.github.com>
2024-10-10 12:35:03 +0530

    Added square root, cube, area of rectangle, area of circle features

--- abc.cpp
+++ abc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath> // for square root and pi
 using namespace std;
 
 class Calculator {
@@ -26,6 +27,31 @@ public:
         }
         return a / b;
     }
+
+    // Team B Features
+    // Function to calculate the square root of a number
+    double squareRoot(double a) {
+        if (a < 0) {
+            cout << "Error: Negative input for square root!" << endl;
+            return 0;
+        }
+        return sqrt(a);
+    }
+
+    // Function to calculate the cube of a number
+    double cube(double a) {
+        return a * a * a;
+    }
+
+    // Function to calculate the area of a rectangle
+    double areaRectangle(double length, double width) {
+        return length * width;
+    }
+
+    // Function to calculate the area of a circle
+    double areaCircle(double radius) {
+        return M_PI * radius * radius;  // Using M_PI from cmath for pi value
+    }
 };
 
 int main() {
@@ -36,11 +62,14 @@ int main() {
     cout << "Enter first number: ";
     cin >> num1;
 
-    cout << "Enter operation (+, -, *, /): ";
+    cout << "Enter operation (+, -, *, /, r, c, S, A): ";
+    cout << "\nr for square root, c for cube, S for area of rectangle, A for area of circle" << endl;
     cin >> operation;
 
-    cout << "Enter second number: ";
-    cin >> num2;
+    if (operation != 'r' && operation != 'c' && operation != 'A') { // Square root, cube, and area of circle need one input
+        cout << "Enter second number: ";
+        cin >> num2;
+    }
 
     switch (operation) {
         case '+':
@@ -54,6 +83,24 @@ int main() {
             break;
         case '/':
             cout << "Result: " << calc.divide(num1, num2) << endl;
+            break;
+        case 'r':
+            cout << "Result: " << calc.squareRoot(num1) << endl;
+            break;
+        case 'c':
+            cout << "Result: " << calc.cube(num1) << endl;
+            break;
+        case 'S':
+            cout << "Enter length: ";
+            cin >> num1;
+            cout << "Enter width: ";
+            cin >> num2;
+            cout << "Area of Rectangle: " << calc.areaRectangle(num1, num2) << endl;
+            break;
+        case 'A':
+            cout << "Enter radius: ";
+            cin >> num1;
+            cout << "Area of Circle: " << calc.areaCircle(num1) << endl;
             break;
         default:
             cout << "Invalid operation!" << endl;
